827492a2a79b7138a06077842eeb81afea95e39c
John Rey <adioskid@mail.ru>
2023-08-08 22:26:12 +0300

    weather

--- src/main/java/com/app/weather/controller/WeatherController.java
+++ src/main/java/com/app/weather/controller/WeatherController.java
@@ -3,6 +3,7 @@ package com.app.weather.controller;
 import com.app.weather.model.Weather;
 import com.app.weather.service.WeatherService;
 import jakarta.annotation.Resource;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,13 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherController {
+
     @Resource
     private WeatherService weatherService;
-
+    @Resource
+    private ConfigurableApplicationContext context;
 
     @GetMapping("weather/{city}")
     public ResponseEntity<Weather> getWeather(@PathVariable String city) {
         Weather weather = weatherService.getWeather(city);
         return ResponseEntity.ok(weather);
     }
+
+    @GetMapping("/exit")
+    public void closeApplication() {
+        context.close();
+
+
+    }
+
 }
